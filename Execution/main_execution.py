0526c40f5e029318f26c509b1a5d24a1e6924475
Tananashvilii <112684839+Tananashvilii@users.noreply.github.com>
2024-11-21 20:40:08 +0400

    sss

--- Execution/main_execution.py
+++ Execution/main_execution.py
@@ -53,8 +53,11 @@ def monitor_zscore(ticker_1, ticker_2, direction_1, direction_2, stop_loss, desi
     # Check if position is still active
     side_1, size_1, change_percent_1 = get_position_info(ticker_1, True)
     side_2, size_2, change_percent_2 = get_position_info(ticker_2, True)
-    change_percent = round(float(change_percent_1) + float(change_percent_2), 1)
-    change_percent *= 10
+    try:
+        change_percent = round(float(change_percent_1) + float(change_percent_2), 1)
+        change_percent *= 10
+    except ValueError:
+        change_percent = 0
 
     if float(size_1) == 0 or float(size_2) == 0:
 
